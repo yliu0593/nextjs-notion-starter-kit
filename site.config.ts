18b1ff1598e4cda00b9c31cac8fdb1d0c853422b
Youlin Liu <yliu0593@gmail.com>
2023-03-01 21:42:41 -0500

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -52,14 +52,14 @@ export default siteConfig({
   // important pages
   navigationStyle: 'default'
   // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
+  navigationLinks: [
+   {
        title: 'About',
        pageId: '216b0229b5e342d99d44ddf3b192dcd9'
-  //   },
+     }
   //   {
   //     title: 'Contact',
   //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
   //   }
-  // ]
+   ]
 })
