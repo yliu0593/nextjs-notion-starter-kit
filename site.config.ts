32e7fbb711b7c34900171b016b780f745c55d7e1
Youlin Liu <yliu0593@gmail.com>
2023-03-01 21:19:30 -0500

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,9 +9,9 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: "youlin's blog",
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'youlin',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
